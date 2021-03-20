72e16ec8d9d0ee0e8f17cb595386543e5efb6006
Manfrin92 <manfredd_10@hotmail.com>
2021-03-20 09:17:43 -0300

    feat: Repo and mocking repo

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil", Platform="Pan"},
+                new Command{Id=1, HowTo="Boil an plate", Line="Boil", Platform="Plate"},
+                new Command{Id=2, HowTo="Boil an t-shirt", Line="Boil", Platform="Pan"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil", Platform="Pan"};
+        }
+    }
+}
